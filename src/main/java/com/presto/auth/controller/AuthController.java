ec2aa25d6f0c67a3507d2143ef996c9ed90c2ddb
padi-kodwo <opd456asp>
2021-10-04 16:48:04 +0000

    - added path to auth controller

--- src/main/java/com/presto/auth/controller/AuthController.java
+++ src/main/java/com/presto/auth/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +24,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-@RestController("/auth")
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-
 
     private final UserServiceImpl userService;
     private final ModelMapper modelMapper;
